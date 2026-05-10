164372eb8300ebc59b514ff683165b2f6bea2ca6
HowXu <howxu366@outlook.com>
2026-05-10 20:36:17 +0800

    feat(app): add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import Fastify from "fastify";
 import cachedData from "./sentences.json"
+import cors from "@fastify/cors"
+
 
 
 const app = Fastify({
     logger: true,
+});
+
+app.register(cors, {
+    // 允许所有域名访问，或者你可以指定具体的域名
+    // origin: ["https://your-frontend.com"], 
+    origin: true, // 这样写会自动匹配请求方的 Origin
 });
 
 const len: number = cachedData.length;
